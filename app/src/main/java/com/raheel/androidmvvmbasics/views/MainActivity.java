91823bdcace65aefa990ebf1c6bfe0457d6b188e
mraheeldev <m.raheeldev@gmail.com>
2022-03-07 12:39:03 +0500

    Initial commit

---
+++ app/src/main/java/com/raheel/androidmvvmbasics/views/MainActivity.java
@@ -0,0 +1,34 @@
+package com.raheel.androidmvvmbasics.views;
+
+
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+import com.raheel.androidmvvmbasics.R;
+import com.raheel.androidmvvmbasics.databinding.ActivityMainBinding;
+import com.raheel.androidmvvmbasics.viewmodels.LoginViewModel;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(new LoginViewModel());
+
+        activityMainBinding.executePendingBindings();
+
+    }
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
